f66235216215daddb0f746b4b424ee7620f3aebf
temidayoakinyemi <temidayoakinyemi886@gmail.com>
2025-06-13 10:33:00 -0600

    OFSPCE

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,12 +6,13 @@ import menu_icon from "../../assets/menu-icon.png";
 const Navbar = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
-    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    setMobileMenu(!mobileMenu);
   };
+
   return (
     <nav className="container">
-      <img src={logo} alt="" className="logo" />
-      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
+      <img src={logo} alt="Logo" className="logo" />
+      <ul className={`mobile-nav ${mobileMenu ? "" : "hide-mobile-menu"}`}>
         <li>Home</li>
         <li>Pricing</li>
         <li>Api Docs</li>
@@ -20,7 +21,12 @@ const Navbar = () => {
         </li>
         <li>Sign Up</li>
       </ul>
-      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
+      <img
+        src={menu_icon}
+        alt="Menu Icon"
+        className="menu-icon"
+        onClick={toggleMenu}
+      />
     </nav>
   );
 };
